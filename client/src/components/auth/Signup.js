fb362b87fc922bff4b8e7490122932e90fcb0f2f
yongchengzhu <yongchengzhu002@gmail.com>
2019-03-31 00:09:19 -0400

    Added redux form and used Field component inside of Signup component.

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { reduxForm, Field } from 'redux-form';
 
 class Signup extends React.Component {
   render() {
@@ -6,16 +7,19 @@ class Signup extends React.Component {
       <form>
         <fieldset>
           <label>Name</label>
+          <Field name="name" type="text"  component="input" autoComplete="off" />
         </fieldset>        
         <fieldset>
           <label>Email</label>
+          <Field name="email" type="text" component="input" autoComplete="off" />
         </fieldset>
         <fieldset>
           <label>Password</label>
+          <Field name="password" type="password"  component="input" autoComplete="off" />
         </fieldset>
       </form>
     );
   }
 }
 
-export default Signup;
+export default reduxForm({ form: 'signup' })(Signup);
