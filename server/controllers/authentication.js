63a5d7adfa3f25ff26bd99161c94ec332490164c
yongchengzhu <yongchengzhu002@gmail.com>
2019-03-30 21:12:28 -0400

    Whenever user successfully signup, our server sends a response back with a token.

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,5 +1,31 @@
-const User = require('../models/user');
+//------------------------------------------------------------------------------------------------
+// Internal Dependencies
+//------------------------------------------------------------------------------------------------
 
+const config = require('../config');
+const User   = require('../models/user');
+const jwt    = require('jwt-simple');
+
+// 
+// tokenForUser:
+//   Generates a json web token with the _id from user model. We are encoding the user id with
+//   our secret string from config.js. We want to return a token to the browser whenever user
+//   has successfully signed up or signed in.
+// 
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+//
+// Signup Callback:
+//   Receives a request object with name, email, and password properties. Then, check if all three
+//   fields are filled by the user. Then, it takes the user's email address and look into the
+//   database to check if this email already exists. If it exists, then return error. If not, then
+//   create a new user model instance with all three properties and save it inside of the database.
+//   Finally, after it is successfully saved, return some response back to the user.
+// 
 exports.signup = function(req, res, next) {
   const name = req.body.name;
   const email = req.body.email;
@@ -24,7 +50,7 @@ exports.signup = function(req, res, next) {
 
     user.save(function(err) {
       if (err) { return next(err); }
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 }
