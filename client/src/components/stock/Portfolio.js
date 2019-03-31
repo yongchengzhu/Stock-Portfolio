e982cb9ccedd04c35294535acf240eaa0fe8f4af
yongchengzhu <yongchengzhu002@gmail.com>
2019-03-31 00:49:52 -0400

    Added middleware to Portfolio route. User must be authenticated to get inside.

--- client/src/components/stock/Portfolio.js
+++ client/src/components/stock/Portfolio.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import requireAuth from '../requireAuth';
+
 class Portfolio extends React.Component {
   render() {
     return <div>Portfolio Page</div>
   }
 }
 
-export default Portfolio;
+export default requireAuth(Portfolio);
